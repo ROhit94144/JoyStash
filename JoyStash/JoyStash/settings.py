1a5c5fb4b782048b927ec084c902cfa4b0ba0897
rohitgiri0 <rajendragiri364@gmail.com>
2025-09-09 18:02:52 +0530

    all done

--- JoyStash/JoyStash/settings.py
+++ JoyStash/JoyStash/settings.py
@@ -17,6 +17,8 @@ env=Env()
 Env.read_env()
 ENVIRONMENT=env('ENVIRONMENT', default='production')
 
+PORT = int(os.environ.get("PORT", 8000))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
