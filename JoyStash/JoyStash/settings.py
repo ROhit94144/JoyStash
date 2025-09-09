768896d5b7e16cbf71d97cf22c7e32ba02ca8c6d
rohitgiri0 <rajendragiri364@gmail.com>
2025-09-09 17:29:54 +0530

    deployee setting

--- JoyStash/JoyStash/settings.py
+++ JoyStash/JoyStash/settings.py
@@ -32,7 +32,11 @@ if ENVIRONMENT=='development':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'joystash.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS=['https://joystash.up.railway.app']
+
+
 SITE_ID = 1
 
 # Application definition
